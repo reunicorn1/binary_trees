05eb9442d5dfbb3fb444d6e00d3fefd1401b966d
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-01-30 02:10:01 +0400

    updating the header file

--- binary_trees.h
+++ binary_trees.h
@@ -34,10 +34,7 @@ typedef struct binary_tree_s binary_tree_t;
 /*________ Function Prototypes ________*/
 
 /*== binary_tree_print.c ==*/
-
 void binary_tree_print(const binary_tree_t *tree);
-/*static size_t _height(const binary_tree_t *tree);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);*/
 
 /*== 0-binary_tree_node.c ==*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
