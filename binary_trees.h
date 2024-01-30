6e574c1f932906cb50abee0d3924c24f8410770b
DeborahRise <debie.rise.bookings@gmail.com>
2024-01-30 08:01:10 +0000

    updated the header file with prototypes of Debs tasks

--- binary_trees.h
+++ binary_trees.h
@@ -40,24 +40,53 @@ void binary_tree_print(const binary_tree_t *tree);
 /*== 0-binary_tree_node.c ==*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+
+
+/*== 1-binary_tree_insert_left.c ==*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 /*== 2-binary_tree_insert_right.c ==*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/*== 3-binary_tree_delete.c ==*/
+void binary_tree_delete(binary_tree_t *tree);
 
 /*== 4-binary_tree_is_leaf.c ==*/
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/*== 5-binary_tree_is_root.c ==*/
+int binary_tree_is_root(const binary_tree_t *node);
+
 /*== 6-binary_tree_preorder.c ==*/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
+/*== 7-binary_tree_inorder.c ==*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
 /*== 8-binary_tree_postorder.c ==*/
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/*== 9-binary_tree_height.c ==*/
+size_t binary_tree_height(const binary_tree_t *tree);
 
 /*== 10-binary_tree_depth.c ==*/
 size_t depth(const binary_tree_t *tree, size_t d);
 size_t binary_tree_depth(const binary_tree_t *tree);
 
+/*== 11-binary_tree_size.c ==*/
+size_t binary_tree_size(const binary_tree_t *tree);
+
 /*== 12-binary_tree_leaves.c ==*/
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t leaves(const binary_tree_t *tree, size_t l);
+
+/*== 13-binary_tree_nodes.c ==*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/*== 15-binary_tree_is_full.c ==*/
+int binary_tree_is_full(const binary_tree_t *tree);
+
+/*== 17-binary_tree_sibling.c ==*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif
